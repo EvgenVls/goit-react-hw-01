f134204b1b914fb94b6b20cd025b4fff45c63fbf
EvgenVls <145988014+EvgenVls@users.noreply.github.com>
2024-03-28 17:59:42 +0200

    start task 1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,28 @@
+export const Profile = ({ name, tag, location, image, status }) => {
+  return (
+    <div>
+      <div>
+        <img src={image} alt="User avatar" />
+        <p>{name}</p>
+        <p>{tag}</p>
+        <p>{location}</p>
+      </div>
+      <ul>
+        <li>
+          <span>{status.followers}</span>
+          <span>1000</span>
+        </li>
+
+        <li>
+          <span>{status.views}</span>
+          <span>2000</span>
+        </li>
+
+        <li>
+          <span>{status.likes}</span>
+          <span>3000</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
