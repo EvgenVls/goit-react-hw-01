153aa27b45b256b65411d3827d183784ac05fdea
EvgenVls <145988014+EvgenVls@users.noreply.github.com>
2024-03-29 17:43:45 +0200

    data in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import userData from "./userData.json";
-// import { Profile } from "./components/Profile/Profile";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import userData from "./userData.json";
+import { Profile } from "./components/Profile/Profile";
 import "./App.css";
 
-const user = JSON.parse(userData);
+const userData = {
+  username: "Jacques Gluke",
+  tag: "jgluke",
+  location: "Ocho Rios, Jamaica",
+  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+
+// const user = JSON.parse(userData);
+
 export default function App() {
-  console.log(user);
-  // return {
-  //   // < Profile />;
-  // };
+  return (
+    <div>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      ;
+    </div>
+  );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -9,17 +9,17 @@ export const Profile = ({ name, tag, location, image, status }) => {
       </div>
       <ul>
         <li>
-          <span>{status.followers}</span>
+          <span>status.followers</span>
           <span>1000</span>
         </li>
 
         <li>
-          <span>{status.views}</span>
+          <span>status.views</span>
           <span>2000</span>
         </li>
 
         <li>
-          <span>{status.likes}</span>
+          <span>status.likes</span>
           <span>3000</span>
         </li>
       </ul>
